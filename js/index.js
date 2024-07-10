9865ae9a7af7eaed5f6d586a5252c805ceed36a4
Nefi <neffepito@gmail.com>
2024-07-11 01:48:17 +0800

    Home save

--- js/index.js
+++ js/index.js
@@ -1,2 +1,21 @@
-let data-1
-console.log("Hello")
+let a = 1;
+// document.write(console.log(a));
+let b = "BB";
+// document.write(console.log(b));
+// document.write(Boolean(a && b));
+// document.write(Boolean(a && b && ""));
+let c; // c = a/ c = b, if (a){c = a} else if (b) {c = b} else {c = undefined}
+// document.write(Boolean(c));
+// document.write(c);
+// 定义一个函数来设置 c 的值
+function setC() {
+  if (a) {
+    c = a;
+  } else {
+    c = b;
+  }
+}
+// 调用函数设置 c 的值
+setC();
+// 输出 c 的值
+console.log(c);
